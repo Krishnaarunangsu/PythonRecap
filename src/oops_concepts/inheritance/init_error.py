e443d12f051c3296faaf61490da4143b4dba0478
Krishnaarunangsu <arunangsutech@gmail.com>
2023-07-20 20:45:33 +0530

    Python OOPS Version 3

--- src/oops_concepts/inheritance/init_error.py
+++ src/oops_concepts/inheritance/init_error.py
@@ -1,17 +1,23 @@
 class A:
-    def __init__(self, n='Rahul'):
-        self.name = n
+    def __init__(self, name='Rahul'):
+        self.name = name
 
 
 class B(A):
     """
 
     """
-    def __init__(self, roll):
-        self.roll = roll
 
-object = B(23)
-#object = B('Tom')
-#object = B()
+    def __init__(self, name, roll):
+        self.roll = roll
+        super().__init__(name)
+
+
+# obj_1 = B(23)
+obj_1 = B('Jack', 23)
+obj_2 = A('Tom')
+# object = B()
 # print(object.name)
-print(object.roll)
+print(obj_1.roll)
+print(obj_1.name)
+print(obj_2.name)
